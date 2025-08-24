17bd2340f7088cc2ee0ffdd2ac96817c508b39d6
Asadkhon Rasulov <asadbeka09022003@gmail.com>
2025-08-24 15:11:21 +0500

    added week 5 day 1

---
+++ Week5/Weather + Blog UI Kit/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-blue-500 text-white p-4">
+        <h1 className="text-2xl font-bold">Hello Tailwind v4!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
